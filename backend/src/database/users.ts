59977ef8b324d1a2d01ec4ab19333c5e8579e5d0
mallocaholic <jrmarcondesb@gmail.com>
2023-07-26 18:41:24 -0300

    feat(followers): added user database example

---
+++ backend/src/database/users.ts
@@ -0,0 +1,40 @@
+type User = {
+  id: number;
+  username: string;
+  email: string;
+  followers: number[];
+  following: number[];
+};
+
+const users: User[] = [
+  {
+    id: 1,
+    username: "Alice",
+    email: "alice@example.com",
+    followers: [],
+    following: []
+  },
+  {
+    id: 2,
+    username: "Bob",
+    email: "bob@example.com",
+    followers: [],
+    following: []
+  },
+  {
+    id: 3,
+    username: "Charlie",
+    email: "charlie@example.com",
+    followers: [],
+    following: []
+  },
+  {
+    id: 5,
+    username: "Junior",
+    email: "junior@example.com",
+    followers: [],
+    following: []
+  }
+];
+
+export default users;
