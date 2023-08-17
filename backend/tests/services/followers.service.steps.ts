f6a8a060f827adbcc1602e62c297e3da2852f3e9
Maria <mevm@cin.ufpe.br>
2023-08-17 12:25:02 -0300

    Feat(followers): Added followers services

--- backend/tests/services/followers.service.steps.ts
+++ backend/tests/services/followers.service.steps.ts
@@ -9,6 +9,71 @@ let followResponse: supertest.Response;
 
 defineFeature(feature, test => {
 
+        test('Returns following list', ({ given, when, then, and }) => {
+            given(/^that the system has an existing user with ID "(\d+)"$/, (userid) => {
+                // Verify that the user exists
+                userid = parseInt(userid);
+                const user = users.find(user => user.id === userid);
+                expect(user).not.toBeUndefined();
+            });
+
+            and(/^the system has an existing user with ID "(\d+)"$/, (userid) => {
+                // Verify that the user exists
+                userid = parseInt(userid);
+
+                const user = users.find(user => user.id === userid);
+                expect(user).not.toBeUndefined();
+            });
+
+            and(/^the system has an existing user with ID "(\d+)"$/, (userid) => {
+                // Verify that the user exists
+                userid = parseInt(userid);
+
+                const user = users.find(user => user.id === userid);
+                expect(user).not.toBeUndefined();
+            });
+
+            and(/^the list of following of "(\d+)" contains "(\d+)"$/, (userid1, userid2) => {
+                // Verify that the user is following the other user
+                userid1 = parseInt(userid1);
+                userid2 = parseInt(userid2);
+
+                const user = users.find(user => user.id === userid1);
+                expect(user?.following).toContain(userid2);
+            });
+
+            and(/^the list of following of "(\d+)" contains "(\d+)"$/, (userid1, userid2) => {
+                // Verify that the user is following the other user
+                userid1 = parseInt(userid1);
+                userid2 = parseInt(userid2);
+
+                const user = users.find(user => user.id === userid1);
+                expect(user?.following).toContain(userid2);
+            });
+
+            when(/^a GET request is made to "(.*)"/, async (endpoint_) => {
+                // Make the request
+                followResponse = await request.get(`${endpoint_}`);
+            });
+
+            then(/^the system returns a "(\d+)" status$/, (statusCode) => {
+                // Verify that the response is correct
+                expect(followResponse.status).toEqual(parseInt(statusCode));
+            });
+
+            and(/^the response is a list containing user "(\d+)" and user "(\d+)"$/, (userid1, userid2) => {
+                // Verify that the response is correct
+                userid1 = parseInt(userid1);
+                userid2 = parseInt(userid2);
+
+                let user1 = users.find(user => user.id === userid1);
+                let user2 = users.find(user => user.id === userid2);
+
+                expect(followResponse.body).toContainEqual(user1);
+                expect(followResponse.body).toContainEqual(user2);
+            });
+    });
+
     test('Client follows a user', ({ given, when, then, and }) => {
         let endpoint : string;
 
@@ -321,6 +386,80 @@ defineFeature(feature, test => {
         });
     });
 
+    // Scenario: Returns followers list
+    // Given that the system has an existing user with ID "1"
+    // And the system has an existing user with ID "2"
+    // And the system has an existing user with ID "3"
+    // And the list of followers of "1" contains "2"
+    // And the list of followers of "1" contains "3"
+    // When a GET request is made to "/api/users/1/followers"
+    // Then the system returns a "200" status
+    // And the response is a list containing "2" and "3"
+
+    test('Returns followers list', ({ given, when, then, and }) => {
+        given(/^that the system has an existing user with ID "(\d+)"$/, (userid) => {
+            // Verify that the user exists
+            userid = parseInt(userid);
+            const user = users.find(user => user.id === userid);
+            expect(user).not.toBeUndefined();
+        });
+
+        and(/^the system has an existing user with ID "(\d+)"$/, (userid) => {
+            // Verify that the user exists
+            userid = parseInt(userid);
+
+            const user = users.find(user => user.id === userid);
+            expect(user).not.toBeUndefined();
+        });
+
+        and(/^the system has an existing user with ID "(\d+)"$/, (userid) => {
+            // Verify that the user exists
+            userid = parseInt(userid);
+
+            const user = users.find(user => user.id === userid);
+            expect(user).not.toBeUndefined();
+        });
+
+        and(/^the list of followers of "(\d+)" contains "(\d+)"$/, (userid1, userid2) => {
+            // Verify that the user is following the other user
+            userid1 = parseInt(userid1);
+            userid2 = parseInt(userid2);
+
+            const user = users.find(user => user.id === userid1);
+            expect(user?.followers).toContain(userid2);
+        });
+
+        and(/^the list of followers of "(\d+)" contains "(\d+)"$/, (userid1, userid2) => {
+            // Verify that the user is following the other user
+            userid1 = parseInt(userid1);
+            userid2 = parseInt(userid2);
+
+            const user = users.find(user => user.id === userid1);
+            expect(user?.followers).toContain(userid2);
+        });
+
+        when(/^a GET request is made to "(.*)"/, async (endpoint_) => {
+            // Make the request
+            followResponse = await request.get(`${endpoint_}`);
+        });
+
+        then(/^the system returns a "(\d+)" status$/, (statusCode) => {
+            // Verify that the response is correct
+            expect(followResponse.status).toEqual(parseInt(statusCode));
+        });
+
+        and(/^the response is a list containing "(\d+)" and "(\d+)"$/, (userid1, userid2) => {
+            // Verify that the response is correct
+            userid1 = parseInt(userid1);
+            userid2 = parseInt(userid2);
+            let user1 = users.find(user => user.id === userid1);
+            let user2 = users.find(user => user.id === userid2);
+
+            expect(followResponse.body).toContainEqual(user1);
+            expect(followResponse.body).toContainEqual(user2);
+
+        });
+    });
 
 });
 
@@ -328,9 +467,28 @@ test('Get the number of followers of a user', async() => {
     // Unit test for the getFollowersCount function
     let userid = 2;
     let endpoint = "/api/users/" + String(userid) + "/followers/count";
-    console.log(endpoint);
 
     let response = await request.get(`${endpoint}`);
     expect(response.status).toEqual(200);
     expect(response.body.followersCount).toEqual(users[userid - 1].followers.length);
+});
+
+test('Get the number of following of a user', async() => {
+    // Unit test for the getFollowingCount function
+    let userid = 2;
+    let endpoint = "/api/users/" + String(userid) + "/following/count";
+
+    let response = await request.get(`${endpoint}`);
+    expect(response.status).toEqual(200);
+    expect(response.body.followingCount).toEqual(users[userid - 1].following.length);
+});
+
+test('Get the followers of a user that does not exist', async() => {
+    // Unit test for the getFollowersCount function
+    let userid = 777;
+    let endpoint = "/api/users/" + String(userid) + "/followers";
+
+    let response = await request.get(`${endpoint}`);
+    expect(response.status).toEqual(404);
+    expect(response.body.message).toEqual("User not found");
 });
