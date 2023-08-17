db0035d970c7f9f8bfdb1d243e786f0d749d8db7
Victor Kammerer <vko@cin.ufpe.br>
2023-08-17 09:53:46 -0300

    pfv vai

--- frontend/cypress/e2e/tests/common/common.ts
+++
@@ -1,9 +0,0 @@
-import { Given } from '@badeball/cypress-cucumber-preprocessor';
-
-const HOME = 'home';
-
-Given('o usuário está na página {string}', (page: string) => {
-    if (page === HOME) page = '/';
-
-    cy.visit(page);
-});
