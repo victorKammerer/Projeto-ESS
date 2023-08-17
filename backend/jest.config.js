6641f17f1ebc78ee23c3a3a92258e1dee2a8c2cd
Mathbonc <mjbo@cin.ufpe.br>
2023-08-17 01:30:08 -0300

    fix(followers): fixed file name from spec to steps

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
   testEnvironment: 'node',
   moduleFileExtensions: ['js', 'json', 'ts'],
   rootDir: '',
-  testRegex: '.spec.ts$',
+  testRegex: '.steps.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
