6ec0f95e7c98128826a5fc6f207ff0a114580ac1
Mathbonc <mjbo@cin.ufpe.br>
2023-08-17 02:06:53 -0300

    fix(utils): Removed unused function

--- backend/src/routes/utils.ts
+++ backend/src/routes/utils.ts
@@ -1,73 +1,31 @@
-import path from 'path';
+import { User } from '../database/users';
 
-const fs = require('fs');
-const DATABASE_PATH = path.join(__dirname, '../database/users.json');
-
-//Read JSON files
-export function readUsers(){
-    try {
-        const dados = fs.readFileSync(DATABASE_PATH, 'utf8');
-        return JSON.parse(dados);
-    } catch (err) {
-        console.log("File read failed:", err);
-        return;
-    }
-}
-
-//Write JSON files
-export function writeUsers(users : any) {
-    fs.writeFileSync(DATABASE_PATH, JSON.stringify(users, null, 2), 'utf8');
-}
-
-//Delete User
-export function deleteUser(id : number, users : any) {
-    const userArray = Object.values(users);
-    const index = userArray.findIndex((user : any) => user.id === id);
-
-    if (index !== -1) {
-        try{
-            userArray.splice(index,1);
-            writeUsers(userArray);
-            return; 
-        }catch(err){
-            console.log("File could not be written");
-            return;
-        }
-    } else {
-        console.log('Could not find user')
-        return; 
-    }
-}
+export function createUser(
+    id: number,
+    user: string,
+    email: string,
+    password: string,
+    name: string,
+    lastName: string,
+    pronouns: string = '',
+    bio: string = '',
+    followers: number[] = [],
+    following: number[] = [],
+    blocked: number[] = []
+  ):  User {
+    const newUser: User = {
+      id,
+      user,
+      email,
+      password,
+      name,
+      lastName,
+      pronouns,
+      bio,
+      followers,
+      following,
+      blocked,
+    };
   
-//Getters
-export function getUserByID(id : number, users : any) {
-    try{
-        const foundUser = users.find((users:any) => users.id === id);
-        if(foundUser){
-            return foundUser;
-        }else{
-            console.log("User not found");
-            return;
-        }
-    }catch(err){
-        console.log("ERR", err);
-        return;
-    }
-}
-
-export function getUserByUser(user : string, users : any) {
-    try{
-        const foundUser = users.find((users:any) => users.user === user);
-        if(foundUser){
-            return foundUser;
-        }else{
-            console.log("User not found");
-            return;
-        }
-    }catch(err){
-        console.log("ERR", err);
-        return;
-    }
-}
-
-  
+    return newUser;
+  }
