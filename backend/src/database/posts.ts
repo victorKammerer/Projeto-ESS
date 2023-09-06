b060a962d312adcd3e8db23ff3339941d8f4d187
mrcndz <jrmarcondesb@gmail.com>
2023-09-06 19:08:38 -0300

    Feat(*): More itens in post database!

--- backend/src/database/posts.ts
+++ backend/src/database/posts.ts
@@ -72,7 +72,80 @@ let posts: Post[] = [
         title: "Piuiiiiii olha o trem passando",
         description: "Po galera joguei com a amada mt massa",
         comments: 5
+    },
+    {
+        "user_id": 2,
+        "post_id": 5,
+        "status": "active",
+        "date": "2021-05-18",
+        "category": ["FPS", "Shooter"],
+        "game": "Call of Duty: Warzone",
+        "rate": 4,
+        "title": "Dominando o campo de batalha em Warzone",
+        "description": "E aí pessoal, quem mais está jogando Warzone? Vamos formar um esquadrão imbatível!",
+        "comments": 6
+    },
+    {
+        "user_id": 2,
+        "post_id": 6,
+        "status": "active",
+        "date": "2021-06-30",
+        "category": ["Racing", "Sports"],
+        "game": "F1 2021",
+        "rate": 4,
+        "title": "Correndo nas pistas em F1 2021",
+        "description": "Olá, fãs de F1! Estou curtindo muito as corridas no F1 2021. Quem mais gosta de pilotar carros de corrida?",
+        "comments": 7
+    },
+    {
+        "user_id": 3,
+        "post_id": 7,
+        "status": "active",
+        "date": "2021-07-25",
+        "category": ["Simulation", "Adventure"],
+        "game": "The Sims 4",
+        "rate": 5,
+        "title": "Minha história no The Sims 4",
+        "description": "Oi pessoal! Estou criando uma história épica no The Sims 4. Vamos compartilhar nossas experiências!",
+        "comments": 4
+    },
+    {
+        "user_id": 3,
+        "post_id": 8,
+        "status": "active",
+        "date": "2021-08-14",
+        "category": ["RPG", "Fantasy"],
+        "game": "Elden Ring",
+        "rate": 5,
+        "title": "Empolgado com Elden Ring",
+        "description": "Elden Ring está chegando! Quem mais está empolgado com este novo RPG?",
+        "comments": 9
+    },
+    {
+        "user_id": 4,
+        "post_id": 9,
+        "status": "active",
+        "date": "2021-09-05",
+        "category": ["Action", "Adventure"],
+        "game": "Horizon Zero Dawn",
+        "rate": 4,
+        "title": "Aloy e suas aventuras em Horizon Zero Dawn",
+        "description": "Olá pessoal! Estou revisitando o mundo de Horizon Zero Dawn com a Aloy. Vamos compartilhar nossas jornadas!",
+        "comments": 3
+    },
+    {
+        "user_id": 4,
+        "post_id": 10,
+        "status": "active",
+        "date": "2021-10-12",
+        "category": ["Strategy", "Simulation"],
+        "game": "Civilization VI",
+        "rate": 5,
+        "title": "Conquistando o mundo em Civilization VI",
+        "description": "Quem mais gosta de construir impérios em Civilization VI? Vamos trocar estratégias!",
+        "comments": 6
     }
+
 ];
 
 export default posts;
