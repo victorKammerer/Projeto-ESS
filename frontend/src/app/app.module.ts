e6e120a0ae9ed15a5967f8bbca18c23fa9a18413
daviyan5 <davi.yan_ma@yahoo.com>
2023-09-12 15:16:53 -0300

    feat(frontend): Adding frontend for lists

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
@@ -14,6 +14,7 @@ import { FeedComponent } from './feed/feed.component';
 
 import { HomeModule } from './home/home.module';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { GameListComponent } from './game-list/game-list.component';
 
 @NgModule({
     declarations: [AppComponent, HistoricListComponent, NotFoundComponent],
