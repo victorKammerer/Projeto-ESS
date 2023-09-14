44e24d5427bac8e5b24dbc4f4ae1707cee932fd7
Victor Kammerer <vko@cin.ufpe.br>
2023-09-14 07:23:30 -0300

    tbm nn sei

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
@@ -11,15 +11,16 @@ import { HistoricListComponent } from './historic-list/historic-list.component';
 import { FollowersComponent } from './followers/followers.component';
 import { ProfileModule } from './profile/profile.module';
 import { FeedComponent } from './feed/feed.component';
-
+import { CommonModule } from '@angular/common';
 import { HomeModule } from './home/home.module';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { GameListComponent } from './game-list/game-list.component';
-import { SignupComponent } from './signup/signup.component';
+import { SignupComponent } from './signup/signup.component';import { DatePipe } from '@angular/common';
 
 @NgModule({
-    declarations: [AppComponent, HistoricListComponent, NotFoundComponent, SignupComponent],
+    declarations: [AppComponent, HistoricListComponent, NotFoundComponent, SignupComponent, GameListComponent],
     imports: [
+        CommonModule,
         FormsModule,
         BrowserModule,
         HttpClientModule,
@@ -29,7 +30,7 @@ import { SignupComponent } from './signup/signup.component';
         BrowserAnimationsModule,
         ProfileModule,
     ],
-    providers: [],
+    providers: [DatePipe],
     bootstrap: [AppComponent],
     schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
